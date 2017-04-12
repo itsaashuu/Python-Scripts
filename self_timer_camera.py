a8cd1731cd28a8d2ec7e88a39f9744c421a76218
Ashutosh Maheshwari <theashutoshmaheshwari@gmail.com>
2017-04-12 12:04:31 +0530

    Add files via upload

---
+++ self_timer_camera.py
@@ -0,0 +1,23 @@
+'''
+    Python multiple image capture
+    By : Ashutosh Maheshwari
+'''
+import pygame.camera
+import pygame.image
+import time
+pygame.camera.init()
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+print("SELF TIMER\nEnter the time duration in seconds: ");
+num = int(input())
+while num > 0:
+    if num <= 3:
+        print(str(num)+" seconds left\n")
+    if num==1:
+        print("Say CHEESE\n")
+    time.sleep(1)
+    num -= 1
+time.sleep(3)
+cam.start()
+img = cam.get_image()
+pygame.image.save(img, "self_timer.jpg")
+pygame.camera.quit()
